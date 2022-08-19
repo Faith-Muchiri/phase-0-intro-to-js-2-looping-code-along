04b743a444fec92c6f180f7774c692d13de059e4
Faith-Muchiri <faith.wambui@student.moringaschool.com>
2022-08-19 07:14:55 +0300

    modified assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const event = "surprise";
+
+ 
+ function writeCards( names, event ) {
+  let personalCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    personalCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return personalCards
+}
+
+
+function countDown(n){
+    let i=n;
+    while(i>=0){
+       
+        console.log(i);
+        i-=1;
+    }
+
+}
